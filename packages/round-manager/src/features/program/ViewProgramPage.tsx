a47c50326f2f1bac6950487a08353646dd82ddf7
Shavina Chau <shavina.chau@gmail.com>
2022-08-02 10:20:51 -0500

    WIP

--- packages/round-manager/src/features/program/ViewProgramPage.tsx
+++ packages/round-manager/src/features/program/ViewProgramPage.tsx
@@ -1,5 +1,5 @@
 import { useNavigate, useParams, Link } from "react-router-dom"
-import { ArrowLeftIcon, ArrowNarrowRightIcon, PencilIcon, UserIcon } from "@heroicons/react/solid"
+import {ArrowLeftIcon, ArrowNarrowRightIcon, PencilIcon, PlusIcon, PlusSmIcon, UserIcon} from "@heroicons/react/solid"
 import { RefreshIcon } from "@heroicons/react/outline"
 
 import { Button } from "../common/styles"
@@ -7,7 +7,9 @@ import { useWallet } from "../common/Auth"
 import { useListProgramsQuery } from "../api/services/program"
 import { useListRoundsQuery } from "../api/services/round"
 import Navbar from "../common/Navbar"
+import Footer from "../common/Footer";
 
+const abbreviateAddress = (address: string) => `${address.slice(0,8)}...${address.slice(-4)}`
 
 export default function ViewProgram() {
   const { id } = useParams()
@@ -70,83 +72,70 @@ export default function ViewProgram() {
   return (
     <>
       <Navbar programCta={true} />
-      <div className="container mx-auto h-screen px-4 py-16">
-        <header>
-          <div className="mb-7 flex justify-between">
+      <div className="container flex flex-col w-screen h-screen">
+        <header className="border-b">
+          <Link to={`/`}>
+            <ArrowLeftIcon className="h-6 w-6 mr-3 mt-1" aria-hidden="true" />
+          </Link>
+          <div className="mb-2 flex justify-between">
             <div className="flex flex-rows">
-              <Link to={`/`}>
-                <ArrowLeftIcon className="h-6 w-6 mr-3 mt-1" aria-hidden="true" />
-              </Link>
 
               <h1 className="text-3xl sm:text-[32px]">
-                Program Details
+                {program?.metadata?.name || "Program Details"}
               </h1>
             </div>
-
-            {/* TODO: Edit Form */}
-            <Button
-              type="button"
-              $variant="outline"
-              className="inline-flex float-right py-2 px-4 text-sm text-grey-400"
-              onClick={goBack}>
-              <PencilIcon className="h-5 w-5 mr-1" aria-hidden="true" />
-              Edit
-            </Button>
+          </div>
+          <div className="flex flex-row">
+            {
+              program?.operatorWallets.map((operatorWallet, index) =>
+                <div className="bg-white text-grey-500 pb-2 pr-5" key={index}>
+                  <UserIcon className="inline h-4 w-4 text-gray-500 mr-1" />
+                  <span className="text-sm text-gray-500" key={index}>{abbreviateAddress(operatorWallet)}</span>
+                </div>
+              ) || (
+                <p className="text-gray-500 text-sm">
+                  Fetching operator wallets...
+                </p>
+              )
+            }
           </div>
         </header>
 
-        <main>
+        <main className="flex-grow">
           <div>
             <div className="sm:flex sm:justify-between">
-              <div className="sm:basis-3/4 sm:mr-3">
-                {/* TODO: background */}
-                <img src="https://storageapi.fleek.co/thelostone-mc-team-bucket/1500x500-1.jpg" aria-hidden="true" alt="program image" />
-              </div>
+              {/*<div className="sm:basis-3/4 sm:mr-3">*/}
+              {/*  /!* TODO: background *!/*/}
+              {/*  <img src="https://storageapi.fleek.co/thelostone-mc-team-bucket/1500x500-1.jpg" aria-hidden="true" alt="program image" />*/}
+              {/*</div>*/}
 
-              <div className="sm:basis-1/4 text-center border px-8 sm:ml-3">
-                <RefreshIcon className="h-11 w-11 mt-8 mx-auto" aria-hidden="true"></RefreshIcon>
-                <h3 className="text-[16px] my-3">
-                  Grant Round
-                </h3>
-                <p className="text-gray-500 text-xs">
-                  Manage date details and acceptance criteria for your Grant Program Round.
-                </p>
-                <Link to={`/round/create?programId=${program?.id}`}>
-                  <Button className="my-4">
-                    Create Round
-                  </Button>
-                </Link>
-              </div>
+
 
             </div>
-
-            <h1 className="text-3xl my-7">
-              {program?.metadata?.name || "..."}
-            </h1>
 
           </div>
 
           <div className="sm:flex sm:justify-between">
-            <div className="sm:basis-3/4 sm:mr-3">
-              <h2 className="text-sm mb-3">Operators</h2>
-              <div className="grid sm:grid-cols-3 gap-2">
-                {
-                  program?.operatorWallets.map((operatorWallet, index) =>
-                    <div className="bg-white text-grey-500 border rounded py-2 px-5 truncate block" key={index}>
-                      <UserIcon className="inline-flex h-4 w-4 text-black mr-1" />
-                      <span className="text-xs text-gray-500" key={index}>{operatorWallet}</span>
-                    </div>
-                  ) || (
-                    <p className="text-gray-500 text-sm">
-                      Fetching operator wallets...
-                    </p>
-                  )
-                }
-              </div>
+            <div className="sm:mr-3">
+              {/*<h2 className="text-sm mb-3">Operators</h2>*/}
+              {/*<div className="grid sm:grid-cols-3 gap-2">*/}
+              {/*  {*/}
+              {/*    program?.operatorWallets.map((operatorWallet, index) =>*/}
+              {/*      <div className="bg-white text-grey-500 border rounded py-2 px-5 truncate block" key={index}>*/}
+              {/*        <UserIcon className="inline-flex h-4 w-4 text-black mr-1" />*/}
+              {/*        <span className="text-xs text-gray-500" key={index}>{operatorWallet}</span>*/}
+              {/*      </div>*/}
+              {/*    ) || (*/}
+              {/*      <p className="text-gray-500 text-sm">*/}
+              {/*        Fetching operator wallets...*/}
+              {/*      </p>*/}
+              {/*    )*/}
+              {/*  }*/}
+              {/*</div>*/}
 
-              <p className="text-gray-500 border-b text-sm py-4">
-                You can't edit operator wallets after the round is deployed.
-              </p>
+              {/*<p className="text-gray-500 border-b text-sm py-4">*/}
+              {/*  You can't edit operator wallets after the round is deployed.*/}
+              {/*</p>*/}
 
 
               <h2 className="text-sm my-4">My Rounds</h2>
@@ -158,7 +147,25 @@ export default function ViewProgram() {
             </div>
             <div className="sm:basis-1/4 text-center sm:ml-3"></div>
           </div>
+
+          <div className="flex justify-center">
+            <div className="text-center px-8 sm:ml-3 w-96">
+              <RefreshIcon className="h-12 w-12 mt-8 mx-auto bg-zinc-100 rounded-full p-3" aria-hidden="true"></RefreshIcon>
+              <h3 className="text-2xl mt-8 mb-2">
+                My Rounds
+              </h3>
+              <p className="text-gray-500 text-sm">
+                Manage date details and acceptance criteria for your Grant Program Round.
+              </p>
+              <Link to={`/round/create?programId=${program?.id}`}>
+                <Button className="my-4 px-4 mt-10">
+                  <PlusIcon className="h-4 w-4 inline-flex -translate-y-0.5" aria-hidden="true" /> Create round
+                </Button>
+              </Link>
+            </div>
+          </div>
         </main>
+        <Footer />
       </div>
     </>
   )
